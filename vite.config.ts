b106479aeef769075f79cc940bfe6df3b26b1e4c
zacmonroe <zmonroe0820@gmail.com>
2023-09-25 13:55:56 -0400

    chore(deps): add react plugin(?)

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { qwikReact } from "@builder.io/qwik-react/vite";
 
 export default defineConfig(() => {
   return {
-		optimizeDeps: { include: ['@auth/core'] },
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    optimizeDeps: { include: ["@auth/core"] },
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikReact()],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
