b98cb80db2d1d141fc609bd3e042c31fa0f672d0
zacmonroe <zmonroe0820@gmail.com>
2023-09-25 15:28:29 -0400

    feat(CMS): using builder io

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,18 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { qwikReact } from "@builder.io/qwik-react/vite";
+import { builderDevTools } from "@builder.io/dev-tools/vite";
 
 export default defineConfig(() => {
   return {
     optimizeDeps: { include: ["@auth/core"] },
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikReact()],
+    plugins: [
+      builderDevTools(),
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      qwikReact(),
+    ],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
