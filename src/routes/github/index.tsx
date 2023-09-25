be5f67b25c9e0ab7c1ff3e5fe3e752d92b4e3828
zacmonroe <zmonroe0820@gmail.com>
2023-09-25 13:44:08 -0400

    rm: rm demos

--- src/routes/github/index.tsx
+++ src/routes/github/index.tsx
@@ -1,31 +1,9 @@
-import type { QRL } from "@builder.io/qwik";
-import {
-	component$,
-	useSignal,
-	useTask$,
-	implicit$FirstArg,
-} from "@builder.io/qwik";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 import { server$ } from "@builder.io/qwik-city";
 
-const serverLog = server$(function (...args: unknown[]) {
-	console.log.apply(console, args);
-});
-
-// "QRL is Qwik URL, qute"
-export const demoQrl = async (fn: QRL<() => string>) => {
-	// Can use useTask$ if demo$ is used within a component$ !
-	console.log(await fn());
-};
-
-export const demo$ = implicit$FirstArg(demoQrl);
-
 export default component$(() => {
-	demo$(() => "abc");
-
 	const filter = useSignal("");
 	const debouncedFilter = useSignal("");
-
-	serverLog(1, 2, 3);
 
 	// Running the effect on the server so that we can learn about the tracked
 	// value, if it changes. Establishes a connection
